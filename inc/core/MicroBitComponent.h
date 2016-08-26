8c8ef9f337bad715011de5694716a7e57452ee8a
gfsommer <sommer@delta-systems.de>
2016-08-26 17:56:52 +0200

    Update MicroBitComponent.h

--- inc/core/MicroBitComponent.h
+++ inc/core/MicroBitComponent.h
@@ -4,6 +4,10 @@ The MIT License (MIT)
 Copyright (c) 2016 British Broadcasting Corporation.
 This software is provided by Lancaster University by arrangement with the BBC.
 
+Modifications Copyright (c) 2016 Calliope GbR
+Modifications are provided by DELTA Systems (Georg Sommer) - Thomas Kern
+und Björn Eberhardt GbR by arrangement with Calliope GbR. 
+
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
 to deal in the Software without restriction, including without limitation
@@ -36,7 +40,7 @@ DEALINGS IN THE SOFTWARE.
 #define MICROBIT_ID_COMPASS             5
 #define MICROBIT_ID_DISPLAY             6
 
-//EDGE connector events
+//EDGE connector events -> changed!
 #define MICROBIT_IO_PINS                20
 
 #define MICROBIT_ID_IO_P0               7           //P0 is the left most pad (ANALOG/DIGITAL)
@@ -47,15 +51,9 @@ DEALINGS IN THE SOFTWARE.
 #define MICROBIT_ID_IO_P5               12          //COL2 (ANALOG/DIGITAL)
 #define MICROBIT_ID_IO_P6               13          //ROW2
 #define MICROBIT_ID_IO_P7               14          //ROW1
-#define MICROBIT_ID_IO_P8               15          //PIN 18
 #define MICROBIT_ID_IO_P9               16          //ROW3
 #define MICROBIT_ID_IO_P10              17          //COL3 (ANALOG/DIGITAL)
 #define MICROBIT_ID_IO_P11              18          //BTN_B
-#define MICROBIT_ID_IO_P12              19          //PIN 20
-#define MICROBIT_ID_IO_P13              20          //SCK
-#define MICROBIT_ID_IO_P14              21          //MISO
-#define MICROBIT_ID_IO_P15              22          //MOSI
-#define MICROBIT_ID_IO_P16              23          //PIN 16
 #define MICROBIT_ID_IO_P19              24          //SCL
 #define MICROBIT_ID_IO_P20              25          //SDA
 
@@ -67,6 +65,14 @@ DEALINGS IN THE SOFTWARE.
 #define MICROBIT_ID_RADIO_DATA_READY    30
 #define MICROBIT_ID_MULTIBUTTON_ATTACH  31
 #define MICROBIT_ID_SERIAL              32
+
+#define CALLIOPE_ID_IO_P0				33
+#define CALLIOPE_ID_IO_P7				34
+#define CALLIOPE_ID_IO_P8				35
+#define CALLIOPE_ID_IO_P9				36
+#define CALLIOPE_ID_IO_P13				37
+#define CALLIOPE_ID_IO_P14				38
+#define CALLIOPE_ID_IO_P15				39		
 
 #define MICROBIT_ID_MESSAGE_BUS_LISTENER            1021          // Message bus indication that a handler for a given ID has been registered.
 #define MICROBIT_ID_NOTIFY_ONE                      1022          // Notfication channel, for general purpose synchronisation
@@ -81,30 +87,23 @@ DEALINGS IN THE SOFTWARE.
   *
   * All components should inherit from this class.
   *
-  * If a component requires regular updates, then that component can be added to the 
-  * to the systemTick and/or idleTick queues. This provides a simple, extensible mechanism
-  * for code that requires periodic/occasional background processing but does not warrant
-  * the complexity of maintaining its own thread. 
+  * If a component requires regular updates, then you should add the component
+  * to the systemTick and idleTick queues.
   *
-  * Two levels of support are available. 
+  * The system timer will call systemTick() once the component has been added to
+  * the array of system components using system_timer_add_component. This callback
+  * will be in interrupt context.
   *
-  * systemTick() provides a periodic callback during the
-  * micro:bit's system timer interrupt. This provides a guaranteed periodic callback, but in interrupt context
-  * and is suitable for code with lightweight processing requirements, but strict time constraints.
-  * 
-  * idleTick() provides a periodic callback whenever the scheduler is idle. This provides occasional, callbacks
-  * in the main thread context, but with no guarantees of frequency. This is suitable for non-urgent background tasks.
-  *
-  * Components wishing to use these facilities should override the systemTick and/or idleTick functions defined here, and
-  * register their components using system_timer_add_component() fiber_add_idle_component() respectively.
-  *
+  * The idle thread will call idleTick() once the component has been added to the array
+  * of idle components using fiber_add_idle_component. Updates are determined by
+  * the isIdleCallbackNeeded() member function.
   */
 class MicroBitComponent
 {
     protected:
 
-    uint16_t id;                    // Event Bus ID of this component
-    uint8_t status;                 // Component defined state.
+    uint16_t id;                    // Event Bus ID
+    uint8_t status;                 // keeps track of various component state, and also indicates if data is ready.
 
     public:
 
@@ -122,16 +121,29 @@ class MicroBitComponent
       * the array of system components using system_timer_add_component. This callback
       * will be in interrupt context.
       */
-    virtual void systemTick()
-    {
+    virtual void systemTick(){
+
     }
 
     /**
       * The idle thread will call this member function once the component has been added to the array
-      * of idle components using fiber_add_idle_component. 
+      * of idle components using fiber_add_idle_component. Updates are determined by
+      * the isIdleCallbackNeeded() member function.
       */
     virtual void idleTick()
     {
+
+    }
+
+    /**
+      * When added to the idleThreadComponents array, this function will be called to determine
+      * if and when data is ready.
+      *
+      * @note override this if you want to request to be scheduled as soon as possible.
+      */
+    virtual int isIdleCallbackNeeded()
+    {
+        return 0;
     }
 
     /**
