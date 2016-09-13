365e9b74d720d85bffb88ea938edbc1aaeed3c48
Joerg Wedeck <joerg@wedeck.de>
2016-09-13 22:08:31 +0200

    fixed missing defines and fields

--- inc/drivers/MicroBitPin.h
+++ inc/drivers/MicroBitPin.h
@@ -39,6 +39,8 @@ DEALINGS IN THE SOFTWARE.
 #define IO_STATUS_ANALOG_IN                 0x04        // Pin is Analog in
 #define IO_STATUS_ANALOG_OUT                0x08        // Pin is Analog out
 #define IO_STATUS_TOUCH_IN                  0x10        // Pin is a makey-makey style touch sensor
+#define IO_STATUS_EVENT_ON_EDGE             0x20        // Pin will generate events on pin change
+#define IO_STATUS_EVENT_PULSE_ON_EDGE       0x40        // Pin will generate events on pin change
 #define IO_STATUS_EVENTBUS_ENABLED          0x80        // Pin is will generate events on change
 
 //#defines for each edge connector pin - changed!
@@ -70,6 +72,15 @@ DEALINGS IN THE SOFTWARE.
 #define MICROBIT_PIN_DEFAULT_SERVO_RANGE    2000
 #define MICROBIT_PIN_DEFAULT_SERVO_CENTER   1500
 
+#define MICROBIT_PIN_EVENT_NONE             0
+#define MICROBIT_PIN_EVENT_ON_EDGE          1
+#define MICROBIT_PIN_EVENT_ON_PULSE         2
+#define MICROBIT_PIN_EVENT_ON_TOUCH         3
+
+#define MICROBIT_PIN_EVT_RISE               2
+#define MICROBIT_PIN_EVT_FALL               3
+#define MICROBIT_PIN_EVT_PULSE_HI           4
+#define MICROBIT_PIN_EVT_PULSE_LO           5
 
 /**
   * Pin capabilities enum.
@@ -92,8 +103,8 @@ class MicroBitPin : public MicroBitComponent
 {
     // The mbed object looking after this pin at any point in time (untyped due to dynamic behaviour).
     void *pin;
-
     PinCapability capability;
+    uint8_t pullMode;
 
     /**
       * Disconnect any attached mBed IO from this pin.
@@ -107,6 +118,43 @@ class MicroBitPin : public MicroBitComponent
       * DynamicPwm instance, and if it's not, allocates a new DynamicPwm instance.
       */
     int obtainAnalogChannel();
+
+    /**
+      * Interrupt handler for when an rise interrupt is triggered.
+      */
+    void onRise();
+
+    /**
+      * Interrupt handler for when an fall interrupt is triggered.
+      */
+    void onFall();
+
+    /**
+      * This member function manages the calculation of the timestamp of a pulse detected
+      * on a pin whilst in IO_STATUS_EVENT_PULSE_ON_EDGE or IO_STATUS_EVENT_ON_EDGE modes.
+      *
+      * @param eventValue the event value to distribute onto the message bus.
+      */
+    void pulseWidthEvent(int eventValue);
+
+    /**
+      * This member function will construct an TimedInterruptIn instance, and configure
+      * interrupts for rise and fall.
+      *
+      * @param eventType the specific mode used in interrupt context to determine how an
+      *                  edge/rise is processed.
+      *
+      * @return MICROBIT_OK on success
+      */
+    int enableRiseFallEvents(int eventType);
+
+    /**
+      * If this pin is in a mode where the pin is generating events, it will destruct
+      * the current instance attached to this MicroBitPin instance.
+      *
+      * @return MICROBIT_OK on success.
+      */
+    int disableEvents();
 
     public:
 
@@ -122,7 +170,7 @@ class MicroBitPin : public MicroBitComponent
       * @param name the mbed PinName for this MicroBitPin instance.
       *
       * @param capability the capabilities this MicroBitPin instance should have.
-      *                   (PIN_CAPABILITY_DIGITAL, PIN_CAPABILITY_ANALOG, PIN_CAPABILITY_TOUCH, PIN_CAPABILITY_AD, PIN_CAPABILITY_ALL)
+      *                   (PIN_CAPABILITY_DIGITAL, PIN_CAPABILITY_ANALOG, PIN_CAPABILITY_AD, PIN_CAPABILITY_ALL)
       *
       * @code
       * MicroBitPin P0(MICROBIT_ID_IO_P0, MICROBIT_PIN_P0, PIN_CAPABILITY_ALL);
@@ -148,8 +196,9 @@ class MicroBitPin : public MicroBitComponent
     /**
       * Configures this IO pin as a digital input (if necessary) and tests its current value.
       *
+      *
       * @return 1 if this input is high, 0 if input is LO, or MICROBIT_NOT_SUPPORTED
-      *         if the given pin does not have analog capability.
+      *         if the given pin does not have digital capability.
       *
       * @code
       * MicroBitPin P0(MICROBIT_ID_IO_P0, MICROBIT_PIN_P0, PIN_CAPABILITY_BOTH);
@@ -157,6 +206,21 @@ class MicroBitPin : public MicroBitComponent
       * @endcode
       */
     int getDigitalValue();
+
+    /**
+      * Configures this IO pin as a digital input with the specified internal pull-up/pull-down configuraiton (if necessary) and tests its current value.
+      *
+      * @param pull one of the mbed pull configurations: PullUp, PullDown, PullNone
+      *
+      * @return 1 if this input is high, 0 if input is LO, or MICROBIT_NOT_SUPPORTED
+      *         if the given pin does not have digital capability.
+      *
+      * @code
+      * MicroBitPin P0(MICROBIT_ID_IO_P0, MICROBIT_PIN_P0, PIN_CAPABILITY_BOTH);
+      * P0.getDigitalValue(PullUp); // P0 is either 0 or 1;
+      * @endcode
+      */
+    int getDigitalValue(PinMode pull);
 
     /**
       * Configures this IO pin as an analog/pwm output, and change the output value to the given level.
@@ -297,6 +361,47 @@ class MicroBitPin : public MicroBitComponent
       *         given pin is not configured as an analog output.
       */
     int getAnalogPeriod();
+
+    /**
+      * Configures the pull of this pin.
+      *
+      * @param pull one of the mbed pull configurations: PullUp, PullDown, PullNone
+      *
+      * @return MICROBIT_NOT_SUPPORTED if the current pin configuration is anything other
+      *         than a digital input, otherwise MICROBIT_OK.
+      */
+    int setPull(PinMode pull);
+
+    /**
+      * Configures the events generated by this MicroBitPin instance.
+      *
+      * MICROBIT_PIN_EVENT_ON_EDGE - Configures this pin to a digital input, and generates events whenever a rise/fall is detected on this pin. (MICROBIT_PIN_EVT_RISE, MICROBIT_PIN_EVT_FALL)
+      * MICROBIT_PIN_EVENT_ON_PULSE - Configures this pin to a digital input, and generates events where the timestamp is the duration that this pin was either HI or LO. (MICROBIT_PIN_EVT_PULSE_HI, MICROBIT_PIN_EVT_PULSE_LO)
+      * MICROBIT_PIN_EVENT_ON_TOUCH - Configures this pin as a makey makey style touch sensor, in the form of a MicroBitButton. Normal button events will be generated using the ID of this pin.
+      * MICROBIT_PIN_EVENT_NONE - Disables events for this pin.
+      *
+      * @param eventType One of: MICROBIT_PIN_EVENT_ON_EDGE, MICROBIT_PIN_EVENT_ON_PULSE, MICROBIT_PIN_EVENT_ON_TOUCH, MICROBIT_PIN_EVENT_NONE
+      *
+      * @code
+      * MicroBitMessageBus bus;
+      *
+      * MicroBitPin P0(MICROBIT_ID_IO_P0, MICROBIT_PIN_P0, PIN_CAPABILITY_BOTH);
+      * P0.eventOn(MICROBIT_PIN_EVENT_ON_PULSE);
+      *
+      * void onPulse(MicroBitEvent evt)
+      * {
+      *     int duration = evt.timestamp;
+      * }
+      *
+      * bus.listen(MICROBIT_ID_IO_P0, MICROBIT_PIN_EVT_PULSE_HI, onPulse, MESSAGE_BUS_LISTENER_IMMEDIATE)
+      * @endcode
+      *
+      * @return MICROBIT_OK on success, or MICROBIT_INVALID_PARAMETER if the given eventype does not match
+      *
+      * @note In the MICROBIT_PIN_EVENT_ON_PULSE mode, the smallest pulse that was reliably detected was 85us, around 5khz. If more precision is required,
+      *       please use the InterruptIn class supplied by ARM mbed.
+      */
+    int eventOn(int eventType);
 };
 
 #endif
